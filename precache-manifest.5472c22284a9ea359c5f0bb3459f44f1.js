28d1db8bdde78ca9886e9bf25048d2a44d19bced
Web-dev <webdevmac@users.noreply.github.com>
2019-03-18 22:52:21 -0400

    Updates

---
+++ precache-manifest.5472c22284a9ea359c5f0bb3459f44f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "0c278b210dba0ea0b21c",
+    "url": "/my-app/static/js/main.8b2d377a.chunk.js"
+  },
+  {
+    "revision": "f05a1df3217379e807f2",
+    "url": "/my-app/static/js/2.fba19b92.chunk.js"
+  },
+  {
+    "revision": "0c278b210dba0ea0b21c",
+    "url": "/my-app/static/css/main.ac45ea87.chunk.css"
+  },
+  {
+    "revision": "098f236ed1c48edc3187cdafece7fb21",
+    "url": "/my-app/index.html"
+  }
+];
